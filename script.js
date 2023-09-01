8384bc06849e23e34aafc89696faa44f38dda4ab
Md-RazaAli <muhammadrazaa263@gmail.com>
2023-09-01 02:52:21 -0700

    First Task

---
+++ script.js
@@ -0,0 +1,54 @@
+// <<< CHAPTER NO: 1 >>>
+
+// Question No: 1
+alert("");
+
+// Question No: 2
+alert("Error! Please enter valid password");
+
+// Question No: 3
+alert("Welcome to JS Land..\n Happy Coding!");
+
+// Question No: 4
+alert("Welcome to JS Land..");
+alert("Happy Coding!\n Prevent this page from creating additional dialogs.");
+
+// Question No: 5
+alert("Hello.. I can run JS through my web browser's console");
+
+// CHAPTER NO: 2
+
+// Question No: 1
+var username = " This is User Name";
+alert(username);
+
+// Question No: 2
+var myname = "Muhammad Raza Ali";
+alert(myname);
+
+// Question No: 3
+var message = "Hello World";
+alert(message);
+
+// Question No: 4
+alert("Muhammad Raza");
+alert("18 Years Old");
+alert("Certified Mobile Application Development");
+
+// Question No: 5
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+//  Question No: 6
+var email = "muhammadraza1023@gmail.com";
+alert("My Email address is " + email);
+
+// Question No: 7
+var book = " A Smarter way to learn Javascript";
+alert("I'm trying to learn from the book" + book);
+
+//  Question No: 8
+document.writeln("Yah! I can write HTML content through Javascipt");
+
+//  Question No: 9
+var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(symbol);
